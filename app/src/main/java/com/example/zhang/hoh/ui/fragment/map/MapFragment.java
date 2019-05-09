cc519d64bcdcba8b777ad1208d420b92c53f2c0f
zhang <xiaojiedashuai@162.com>
2019-05-09 23:15:09 +0800

    5.9 list界面基本完成 map界面开始接入

--- app/src/main/java/com/example/zhang/hoh/ui/fragment/map/MapFragment.java
+++ app/src/main/java/com/example/zhang/hoh/ui/fragment/map/MapFragment.java
@@ -5,6 +5,8 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.View;
 
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.MapView;
 import com.example.sdk.base.BasePresenter;
 import com.example.sdk.base.fragment.BaseMVPCompatFragment;
 import com.example.zhang.hoh.R;
@@ -12,8 +14,12 @@ import com.example.zhang.hoh.contract.map.MapContract;
 import com.example.zhang.hoh.presenter.map.MapPresenter;
 import com.example.zhang.hoh.ui.fragment.list.ListFragment;
 
+import butterknife.BindView;
+
 public class MapFragment extends BaseMVPCompatFragment<MapContract.MapPresenter> implements  MapContract.MapView {
 
+    //@BindView(R.id.map_mainmap)
+    private  MapView mapView;
     public static MapFragment newInstance() {
         Bundle args = new Bundle();
         MapFragment fragment = new MapFragment();
@@ -34,6 +40,41 @@ public class MapFragment extends BaseMVPCompatFragment<MapContract.MapPresenter>
 
     @Override
     public void initUI(View view, @Nullable Bundle savedInstanceState) {
+        //得到地图对象
+        AMap aMap=null;
+        if (aMap==null){
+            aMap=mapView.getMap();
+        }
+    }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mapView=getActivity().findViewById(R.id.map_mainmap);
+        mapView.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
     }
 }
